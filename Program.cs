ac7bf132d72a64a6018120e67474409d0c983706
Alex <ulkiorra@gmail.com>
2023-07-06 22:58:54 +0300

    Произведение двух матриц (двумерных массивов)

---
+++ Program.cs
@@ -0,0 +1,76 @@
+﻿// Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+// 2 4 | 3 4 2
+// 3 2 | 3 3 1
+// Результирующая матрица будет:
+// 18 20 8
+// 15 18 7
+
+Console.Clear();
+int ReadInt(string message)
+{
+    System.Console.Write($"{message}: ");
+    string inputedString = Console.ReadLine();
+    if (int.TryParse(inputedString, out int convertedInt))
+    {
+        return convertedInt;
+    }
+    System.Console.WriteLine("Вы ввели не число");
+    Environment.Exit(0);
+    return 0;
+}
+
+int[,] GenerateMatrix(int rows, int cols)
+{
+    int[,] arrayD2 = new int[rows, cols];
+    for (int i = 0; i < arrayD2.GetLength(0); i++)
+    {
+        for (int j = 0; j < arrayD2.GetLength(1); j++)
+        {
+            arrayD2[i, j] = new Random().Next(0, 10);
+        }
+    }
+    return arrayD2;
+}
+
+void ShowMatrix(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write($"{array[i, j]}\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+//Произведение двух матриц
+void MultiplyMatrix(int[,] firstMartrix, int[,] secondMartrix, int[,] resultMatrix)
+{
+  for (int i = 0; i < resultMatrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < resultMatrix.GetLength(1); j++)
+    {
+      int sum = 0;
+      for (int k = 0; k < firstMartrix.GetLength(1); k++)
+      {
+        sum += firstMartrix[i,k] * secondMartrix[k,j];
+      }
+      resultMatrix[i,j] = sum;
+    }
+  }
+}
+
+int rows = ReadInt("Введите количество строк");
+int columns = ReadInt("Введите количество столбцов");
+int[,] matrix1 = GenerateMatrix(rows, columns);
+Console.WriteLine("Первая матрица");
+ShowMatrix(matrix1);
+int[,] matrix2 = matrix1; //Матрицы обязаны быть одинаковыми
+Console.WriteLine("Вторая матрица");
+ShowMatrix(matrix2);
+
+int[,] resultMatrix = new int[rows,columns];
+MultiplyMatrix(matrix1, matrix2, resultMatrix);
+Console.WriteLine($"Произведение первой и второй матриц:");
+ShowMatrix(resultMatrix);
